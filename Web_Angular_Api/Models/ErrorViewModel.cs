6712f889819dabc7a4588406f8aa9f7646badbc8
SumanBangera <sumanrbangera@gmail.com>
2022-10-17 19:17:27 +0530

    Add project files.

---
+++ Web_Angular_Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Angular_Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
